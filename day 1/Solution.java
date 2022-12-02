c84fdd2e5716853275c335c0c7376c61435608dd
techfreakSahil <gm6504@myamu.ac.in>
2022-12-02 21:01:00 +0530

    2starpuzzle

--- day 1/Solution.java
+++ day 1/Solution.java
@@ -1,29 +1,37 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 class Solution{
     public static void main(String args[]) throws Exception{
-        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
-        int calories = 0;
-        int maxCalories = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            int calories = 0;
+            int maxCalories = 0;
 
-        String l;
+            ArrayList<Integer> calList = new ArrayList<Integer>();
 
-        while((l = br.readLine()) !=null){
-            String strCal = l.trim();
-            if(strCal.isBlank()){
-                if(maxCalories<calories){
-                    maxCalories = calories;
+            String l;
+            
+            while((l= br.readLine())!=null){
+                String strCal = l.trim();
+                if(strCal.isBlank()){
+                    if(maxCalories<calories){
+                        maxCalories = calories;
+                    }
+                    calList.add(calories);
+                   calories =0;
+                   continue;
                 }
-               calories =0;
-               continue;
+                calories += Integer.parseInt(strCal);
             }
-            calories += Integer.parseInt(strCal);
-        }
-        if(calories>maxCalories){
-            maxCalories = calories;
+            if(calories>maxCalories){
+                maxCalories = calories;
+            }
+
+            System.out.println(maxCalories);
+            calList.sort((a,b)->b-a);
+            System.out.println(calList.get(0) + calList.get(1) + calList.get(2));
         }
 
-        System.out.println(maxCalories);
     }
 }
